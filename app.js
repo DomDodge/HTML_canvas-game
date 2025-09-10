c41cb89103c1933b413d7eadfef9c34e68697214
Dominic Dodge <dominicdodge84@gmail.com>
2025-09-09 18:36:38 -0600

    basic functionality complete

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let canvas = document.getElementById("gameScreen")
+let canvas = document.getElementById("gameScreen");
+let scoreText = document.getElementById("score-text");
 let ctx;
 
 // game variables
@@ -8,6 +9,11 @@ let speed = 5;
 let direction = "none";
 let nextDirection = "none";
 let tileSize = 50; 
+let collected = false;
+let lastSpace = [200, 350];
+let coords = [800, 350];
+let previousDir = [];
+let score = 0;
 
 // Get Direction
 document.addEventListener('keydown', function(event) {
@@ -64,6 +70,16 @@ function isDead() {
         dead = true;
     }  
 
+    deadList = deadZone();
+    for (let i = 0; i < deadList.length; i++) {
+        let x = Math.round(movementX / tileSize) * tileSize;
+        let y = Math.round(movementY / tileSize) * tileSize;
+
+        if(x === deadList[i][0] && y === deadList[i][1]) {
+            dead = true;
+        }
+    }
+
     return dead;
 }
 
@@ -80,21 +96,81 @@ function drawGrid() {
     }
 }
 
+function spawnAppleCords() {
+    let xDist = (canvas.width / tileSize);
+    let yDist = (canvas.height / tileSize);
+    let randX = Math.floor(Math.random() * xDist);
+    let randY = Math.floor(Math.random() * yDist);
+
+    deadList = deadZone();
+    for (let i = 0; i < deadList.length; i++) {
+        if(randX === deadList[i][0] && randY === deadList[i][1]) {
+            return spawnAppleCords();
+        }
+    }
+
+    collected = false;
+    return [randX * tileSize, randY * tileSize];
+}
+
+function storeDir() {
+    // see if the players direction is a tile different from last direction
+    if(Math.abs(lastSpace[0] - movementX) >= 50 || Math.abs(lastSpace[1] - movementY) >= 50) {
+        lastSpace[0] = movementX;
+        lastSpace[1] = movementY;
+        previousDir.push([movementX, movementY]);
+    }
+}
+
+function generateTail() {
+    for(let i = 1; i < score + 1; i++) {
+        let xVariance = 0;
+        let yVariance = 0;
+
+        lastDir = previousDir.at(-i);
+        ctx.fillStyle = "navy";
+
+        ctx.fillRect(lastDir[0], lastDir[1], tileSize, tileSize);
+    }
+}
+
 function startGame() {
     ctx = canvas.getContext("2d");
     requestAnimationFrame(gameLoop);
+}
+
+function deadZone() {
+    return previousDir.slice(-score, -1);
 }
 
 function gameLoop() {
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    drawGrid(50);
+    drawGrid();
 
-    movement();
-
+    // Renter Cureent Apple
+    coords = collected ? spawnAppleCords() : coords;
     ctx.fillStyle = "#e83b3b";
+    ctx.fillRect(coords[0], coords[1], tileSize, tileSize);
+
+    // Player
+    movement();
+    ctx.fillStyle = "navy";
     ctx.fillRect(movementX, movementY, tileSize, tileSize);
+    // Get the tail
+    generateTail();
+
+    // Check if apple collected
+    if(movementX === coords[0] && movementY === coords[1]) {
+        score += 1;
+        collected = true;
+    }
+
+    // Store Past Directions
+    storeDir();
+
+    scoreText.textContent = "score: " + score;
 
     // Loop forever
     if(isDead() == false) {
